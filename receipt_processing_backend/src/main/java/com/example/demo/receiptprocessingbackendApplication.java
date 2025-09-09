6d6e9c34545062bb3c17136df9b622e524b4f05b
Kavia AI <production@kavia.ai>
2025-09-09 04:30:37 +0000

    CheckPoint - cgf71b0235

---
+++ receipt_processing_backend/src/main/java/com/example/demo/receiptprocessingbackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.receiptprocessingbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class receiptprocessingbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(receiptprocessingbackendApplication.class, args);
+	}
+
+}
---
+++ receipt_processing_backend/src/test/java/com/example/demo/receiptprocessingbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.receiptprocessingbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class receiptprocessingbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
